6bf4507fa1eeccd7674e08c34a38be98b12b15b9
CommName <CommName@users.noreply.github.com>
2018-09-03 22:35:00 +0200

    Load and display image

--- mainwindow/mainwindow.cpp
+++ mainwindow/mainwindow.cpp
@@ -1,14 +1,59 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFileDialog>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scene0=new QGraphicsScene(this);
+    scene1=new QGraphicsScene(this);
+    scene2=new QGraphicsScene(this);
+    ui->imageViewMode0->setScene(scene0);
+    ui->imageViewMode1->setScene(scene1);
+    ui->imageViewMode2->setScene(scene2);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//private functions
+void MainWindow::loadImage(){
+
+   QString path = QFileDialog::getOpenFileName(this,"Chose image","","*.png *.jpg *.bmp");
+   if(path=="")
+       return;
+   imagePath=path;
+   imageMode0= cv::imread(path.toLocal8Bit().constData(),CV_LOAD_IMAGE_COLOR);
+
+   displayImageInfo();
+   displayImageMode0();
+   return;
+}
+void MainWindow::displayImageInfo(){
+
+    ui->label_Height->setText( QString::number(imageMode0.rows));
+    ui->label_Width->setText(QString::number(imageMode0.cols));
+
+}
+
+void MainWindow::displayImageMode0(){
+    cv::Mat temp;
+
+
+    cv::cvtColor(imageMode0,temp,CV_BGR2RGB);
+    QImage output((const uchar *)temp.data,temp.cols,temp.rows,QImage::Format_RGB888);
+    output.bits();
+    scene0->clear();
+    scene0->addPixmap(QPixmap::fromImage(output));
+    ui->imageViewMode0->fitInView(scene0->itemsBoundingRect(),Qt::KeepAspectRatio);
+}
+
+//Slots
+void MainWindow::on_buttonLoad_clicked()
+{
+    loadImage();
+}
+
--- mainwindow/mainwindow.h
+++ mainwindow/mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <opencv2/opencv.hpp>
+#include <QString>
+#include <QGraphicsItem>
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +17,34 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_buttonLoad_clicked();
+
 private:
     Ui::MainWindow *ui;
-};
+
+
+//variable
+private:
+    cv::Mat imageMode0;
+    cv::Mat imageMode1;
+    cv::Mat imageMode2;
+    QString imagePath;
+    QGraphicsScene *scene0;
+    QGraphicsScene *scene1;
+    QGraphicsScene *scene2;
+
+
+//funcions
+private:
+    void loadImage();
+    void displayImageInfo();
+    void displayImageMode0();
+
+
+
+};    
+
 
 #endif // MAINWINDOW_H
